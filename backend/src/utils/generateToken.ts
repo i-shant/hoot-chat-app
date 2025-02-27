d152544b570cadd62de60202a6572f6414e4d49c
Ishant Bhurani <188859462+i-shant@users.noreply.github.com>
2025-02-27 14:55:32 +0530

    update cookie options

--- backend/src/utils/generateToken.ts
+++ backend/src/utils/generateToken.ts
@@ -11,7 +11,7 @@ export default function generateToken(res: Response, userId: string) {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
-    sameSite: "strict", // Prevent CSRF attacks
+    sameSite: "none", // Prevent CSRF attacks with `strict`
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 }
